da56ee0a295d6cee0e20b49bb8513fa1ae1b045e
silentboy04 <fadhliyahya04@gmail.com>
2019-05-15 11:08:07 +0700

    post

---
+++ models/post.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const shortid = require('shortid');
+
+const casing = mongoose.Schema({
+    _id: {
+        type: String,
+        required: true,
+        default: shortid.generate
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    postimage: {
+        type: String,
+        required: true,
+    }
+});
+
+module.exports = mongoose.model('Post', post);
